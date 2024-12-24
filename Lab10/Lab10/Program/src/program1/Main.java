f4530467598fbf7357cb8d38bd0a83485cb55f89
daryasakun <117446420+daryasakun@users.noreply.github.com>
2024-12-24 20:54:22 +0300

    Add files via upload
    Lab10PIAPS

---
+++ Lab10/Lab10/Program/src/program1/Main.java
@@ -0,0 +1,11 @@
+package program1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Project project = new Project();
+    System.out.println("Длительность проекта - " + project.getProjectCompleteANumberOfHours() + " дней");
+    Institute vsu = new Institute();
+    //System.out.println(vsu.address);
+    }
+}
